2af13dd3d1244ac4d9922382315894ec57f3603e
miquecg <git@miguelcasal.net>
2017-02-04 21:57:53 +0100

    Passed first test with one spare bonus

--- src/bowling_game.erl
+++ src/bowling_game.erl
@@ -17,6 +17,15 @@
     code_change/3
 ]).
 
+%% Types
+-type bonus() :: x2.
+
+-record(state, {
+    score = 0 :: 0..300,
+    frame = [],
+    bonus = [] :: [bonus()]
+}).
+
 
 %%===================================================================
 %% API
@@ -45,11 +54,13 @@ stop(PID) ->
 %%===================================================================
 
 init([]) ->
-    {ok, 0}.
+    {ok, #state{}}.
 
 handle_call({roll, KnockedPins}, _From, State) ->
-    Points = State + KnockedPins,
-    {reply, Points, Points};
+    NewState1 = update_score(KnockedPins, State),
+    NewState2 = update_frame(KnockedPins, NewState1),
+    NewState = update_bonus(NewState2),
+    {reply, NewState#state.score, NewState};
 handle_call(_Request, _From, State) ->
     {reply, ok, State}.
 
@@ -64,3 +75,33 @@ terminate(_Reason, _State) ->
 
 code_change(_OldVersion, State, _Extra) ->
     {ok, State}.
+
+
+%%===================================================================
+%% Internal functions
+%%===================================================================
+
+-spec update_score(KnockedPins :: 0..10, State :: #state{}) ->
+    #state{}.
+update_score(KnockedPins, #state{bonus = [], score = Score} = State) ->
+    State#state{score = Score + KnockedPins};
+update_score(KnockedPins, #state{bonus = [x2 | NextBonuses], score = Score} = State) ->
+    BonusedRoll = KnockedPins * 2,
+    State#state{
+        score = Score + BonusedRoll,
+        bonus = NextBonuses
+    }.
+
+-spec update_frame(KnockedPins :: 0..10, State :: #state{}) ->
+    #state{}.
+update_frame(KnockedPins, #state{frame = [_, _]} = State) ->
+    State#state{frame = [KnockedPins]};
+update_frame(KnockedPins, #state{frame = Frame} = State) when is_list(Frame) ->
+    State#state{frame = [KnockedPins | Frame]}.
+
+-spec update_bonus(State :: #state{}) ->
+    #state{}.
+update_bonus(#state{frame = [SecondRoll, FirstRoll], bonus = Bonus} = State) when SecondRoll + FirstRoll =:= 10 ->
+    State#state{bonus = lists:append(Bonus, [x2])};
+update_bonus(#state{} = State) ->
+    State.
